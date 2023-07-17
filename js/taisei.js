356622f33c9372ade1e711cd610786f407207646
Taisei_Yamaguchi <aries0326taisei@gmail.com>
2023-07-16 21:23:36 -0700

    Contact page edited.

--- js/taisei.js
+++ js/taisei.js
@@ -17,3 +17,35 @@ function addClickEvent(element, target) {
     addClickEvent(h2, target);
   });
   
+
+
+  // ボタン要素を取得
+var generalInquiryButton = document.getElementById("general-inquiry-button");
+var feedbackButton = document.getElementById("feedback-button");
+
+// フォーム要素を取得
+var generalInquiryForm = document.getElementById("general-inquiry-form");
+var feedbackForm = document.getElementById("feedback-form");
+
+// GENERAL INQUIRYボタンのクリックイベント
+generalInquiryButton.addEventListener("click", function() {
+  // GENERAL INQUIRYフォームを表示し、FEEDBACKフォームを非表示にする
+  generalInquiryForm.classList.remove("hidden");
+  feedbackForm.classList.add("hidden");
+
+  //GENERAL INQUIRYボタンの色を変える。
+  generalInquiryButton.classList.add("color-change-btn");
+  feedbackButton.classList.remove("color-change-btn");
+
+});
+
+// FEEDBACKボタンのクリックイベント
+feedbackButton.addEventListener("click", function() {
+  // FEEDBACKフォームを表示し、GENERAL INQUIRYフォームを非表示にする
+  feedbackForm.classList.remove("hidden");
+  generalInquiryForm.classList.add("hidden");
+
+  //FEEDBACKボタンの色を変える
+  generalInquiryButton.classList.remove("color-change-btn");
+  feedbackButton.classList.add("color-change-btn");
+});
